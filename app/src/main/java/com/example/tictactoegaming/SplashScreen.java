52302740f770fcf514493ee7b0eee25cbb2927dd
MatthewPietr <pietruchamatthew@gmail.com>
2020-09-21 22:04:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tictactoegaming/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.example.tictactoegaming;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+
+
+    }
+}
